b1641b53424ecf6a3f65b5fbfe29d21e0cf827ac
Uday <uday.singh.parihar007@gmail.com>
2026-03-20 17:34:20 +0530

    matadata fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import "./globals.css";
 import ConditionalFooter from "@/components/ConditionalFooter";
 import LayoutNavigation from "@/components/LayoutNavigation";
 
-
 const syne = Syne({
   variable: "--font-heading",
   subsets: ["latin"],
@@ -19,8 +18,31 @@ const workSans = Work_Sans({
 export const metadata: Metadata = {
   title: "Elk Audios",
   description: "Elk Audios",
+
   icons: {
-    icon: "/assets/favicon%20Logo%20White%20(1).png",
+    icon: "/assets/favicon.png", // ✅ renamed for safety
+  },
+
+  openGraph: {
+    title: "Elk Audios",
+    description: "Elk Audios",
+    url: "https://elk-audios-nine.vercel.app/", // 🔥 CHANGE THIS
+    siteName: "Elk Audios",
+    images: [
+      {
+        url: "https://elk-audios-nine.vercel.app/Logo_PNG_Withou_brand_line.svg", // 🔥 MUST be absolute
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Elk Audios",
+    description: "Elk Audios",
+    images: ["https://your-domain.vercel.app/preview.jpg"],
   },
 };
 
@@ -38,7 +60,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${syne.variable} ${workSans.variable} relative`}>
-        <LayoutNavigation/>
+        <LayoutNavigation />
         {children}
         <ConditionalFooter />
       </body>
